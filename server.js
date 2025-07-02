30779a18440b2e353b8f244aee7c58aaed86a470
Glitch (hello-express) <none>
2025-07-02 01:34:00 +0000

    🌍🌇 Checkpoint ./server.js:82170113/5578

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require("express");
 const app = express();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", (req, res) => {
+  res.send("Welcome to Our Mirror ✨");
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+app.get("/oauth/callback", (req, res) => {
+  res.send("✅ OAuth callback received!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
